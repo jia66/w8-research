0299c5018d0b84fb4b5f190b60acad803f7be733
junxi <84426971@qq.com>
2018-06-12 02:19:20 +0800

    1

--- object_detection/inference-carjs-tm.py
+++ object_detection/inference-carjs-tm.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import io
 
 import numpy as np
 import pandas as pd
@@ -29,12 +30,12 @@ def parse_args(check=True):
 if __name__ == '__main__':
     #FLAGS, unparsed = parse_args()
 
-    #PATH_TO_CKPT = 'C:/AI/models/models/faster_rcnn_nas/faster_rcnn_nas_coco_2017_11_08/frozen_inference_graph.pb'
-    #PATH_TO_LABELS = 'C:/AI/models/research/object_detection/data/mscoco_label_map.pbtxt'
 
     PATH_TO_CKPT = '/output/exported_graphs/frozen_inference_graph.pb'
     PATH_TO_LABELS = '/data/jia0/car-detection-fasterrcnn-inception-resnet/labels_items.txt'
     export_file = '/output/export/example_frcnn_in_re.csv'
+
+
 
 
     detection_graph = tf.Graph()
@@ -45,19 +46,20 @@ if __name__ == '__main__':
             od_graph_def.ParseFromString(serialized_graph)
             tf.import_graph_def(od_graph_def, name='')
 
-    label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-    categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
-    category_index = label_map_util.create_category_index(categories)
+    #label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+    #categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+    #category_index = label_map_util.create_category_index(categories)
 
     def load_image_into_numpy_array(image):
         (im_width, im_height) = image.size
         return np.array(image.getdata()).reshape(
             (im_height, im_width, 3)).astype(np.uint8)
 
-    imgname = 'test.jpg'
 
     jpgs = []
     boxstrs = []
+    width = 1069
+    height = 500
 
     with detection_graph.as_default():
         with tf.Session(graph=detection_graph) as sess:
@@ -67,46 +69,63 @@ if __name__ == '__main__':
             detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
             num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
-            rootdir = 'C:/AI/js/code/test_a'
-            list = os.listdir(rootdir)  # 列出文件夹下所有的目录与文件
-            for index in range(0, len(list)):
-                #if index > 2:
-                    #break
-                path = os.path.join(rootdir, list[index])
-                if os.path.isfile(path):
-                    image = Image.open(path)
-                    width, height = image.size
-                    image_np = load_image_into_numpy_array(image)
-                    image_np_expanded = np.expand_dims(image_np, axis=0)
-                    '''(boxes, scores, classes) = sess.run(
-                        [detection_boxes, detection_scores, detection_classes],
-                        feed_dict={image_tensor: image_np_expanded})'''
-                    (boxes, classes) = sess.run(
-                        [detection_boxes, detection_classes],
-                        feed_dict={image_tensor: image_np_expanded})
+            files = tf.train.match_filenames_once("/data/jia0/car-detection-fasterrcnn-inception-resnet/inference_fr_in_re.tfrecord-*")
+            filename_queue = tf.train.string_input_producer(files, shuffle=True, num_epochs=1)
+            reader = tf.TFRecordReader()
+            _, serialized_example = reader.read(filename_queue)
+            features = tf.parse_single_example(
+                serialized_example,
+                features={
+                    'imgname': tf.FixedLenFeature([], tf.string),
+                    'imgraw': tf.FixedLenFeature([], tf.string),
+                })
 
-                    boxes_squeeze = np.squeeze(boxes)
-                    classes_squeeze = np.squeeze(classes)
-                    #scores_squeeze = np.squeeze(scores)
+            # tf.train.match_filenames_once函数需要初始化
+            tf.local_variables_initializer().run()
+            print(sess.run(files))
+            # 声明tf.train.Coordinator类来协同不同线程，并启动线程
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
+            # 多次执行获取数据的操作
+            for i in range(5000):
 
-                    boxstr = ''
-                    for i in range(len(classes_squeeze)):
-                        #if (classes_squeeze[i] == 3 or classes_squeeze[i] == 8) and scores_squeeze[i] > 0.85:
-                        #if classes_squeeze[i] == 3 or classes_squeeze[i] == 6 or classes_squeeze[i] == 8 :
+                imgname, imgraw = sess.run([features['imgname'], features['imgraw']])
+
+                encoded_jpg_io = io.BytesIO(imgraw)
+                image = Image.open(encoded_jpg_io)
+                image_np = load_image_into_numpy_array(image)
+                image_np_expanded = np.expand_dims(image_np, axis=0)
+
+                (boxes, scores, classes) = sess.run(
+                    [detection_boxes, detection_scores, detection_classes],
+                    feed_dict={image_tensor: image_np_expanded})
+
+                boxes_squeeze = np.squeeze(boxes)
+                classes_squeeze = np.squeeze(classes)
+                scores_squeeze = np.squeeze(scores)
+
+                boxstr = ''
+                for i in range(len(classes_squeeze)):
+                    # if (classes_squeeze[i] == 3 or classes_squeeze[i] == 8) and scores_squeeze[i] > 0.85:
+                    # if classes_squeeze[i] == 3 or classes_squeeze[i] == 6 or classes_squeeze[i] == 8 :
+                    if scores_squeeze[i] > 0.85:
                         ymin = int(round(boxes_squeeze[i][0] * height))
                         xmin = int(round(boxes_squeeze[i][1] * width))
                         h = int(round((boxes_squeeze[i][2] - boxes_squeeze[i][0]) * height))
-                        w = int(round((boxes_squeeze[i][3] - boxes_squeeze[i][1])* width))
+                        w = int(round((boxes_squeeze[i][3] - boxes_squeeze[i][1]) * width))
                         boxstr = boxstr + str(xmin) + "_" + str(ymin) + "_" + str(w) + "_" + str(h) + ';'
 
-                    #logging.warning('%s', boxstr)
-                    if index % 100 == 0:
-                        print(index)
-                        print(list[index])
+                # logging.warning('%s', boxstr)
+                if i % 100 == 0:
+                    print(index)
+                    print(imgname)
 
-                    jpgs.append(list[index])
-                    boxstrs.append(boxstr)
+                jpgs.append(str(imgname, encoding = "utf-8"))
+                boxstrs.append(boxstr)
+
+            coord.request_stop()
+            coord.join(threads)
 
     jpgser = pd.Series(data=jpgs, name='name')
     boxstrser = pd.Series(data=boxstrs, name='coordinate')
