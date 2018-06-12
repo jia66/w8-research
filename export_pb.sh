1ac42f607d8b05954a3b33d76630278ec1458550
junxi <84426971@qq.com>
2018-06-12 08:52:14 +0800

    1

--- export_pb.sh
+++ export_pb.sh
@@ -30,7 +30,7 @@ pipeline_config_path=$output_dir/$config
 #python ./object_detection/train.py --train_dir=$train_dir --pipeline_config_path=$pipeline_config_path
 
 # 导出模型
-python ./object_detection/export_inference_graph.py --input_type image_tensor --pipeline_config_path $pipeline_config_path --trained_checkpoint_prefix $train_dir/model.ckpt-19984  --output_directory $output_dir/exported_graphs
+#python ./object_detection/export_inference_graph.py --input_type image_tensor --pipeline_config_path $pipeline_config_path --trained_checkpoint_prefix $train_dir/model.ckpt-19984  --output_directory $output_dir/exported_graphs
 
 # 在test.jpg上验证导出的模型
 #python ./object_detection/inference.py --output_dir=$output_dir --dataset_dir=$dataset_dir
